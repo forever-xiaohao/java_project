720f092820ebf607d355c8b93e33e3df05a6ea5d
forever-xiaohao <1012958103@qq.com>
2017-03-30 12:35:51 +0800

    test : equals/hashCode/toString
    实现Cloneable接口，实现对象的克隆，以及比较，
    实现的方法有equals(),hashCode(),clone()

--- java_jdk7_ch2/src/java_jdk7_ch2/ComplexNumber.java
+++ java_jdk7_ch2/src/java_jdk7_ch2/ComplexNumber.java
@@ -1,9 +1,66 @@
 package java_jdk7_ch2;
-
-public class ComplexNumber {
+/**
+ * 首先看Object类的equals,hashCode和clone方法的用途
+ * equals方法：比较两个对象是否相等，结果为boolean类型
+ * hashCode方法：获得对象的hash码，结果为int类型。
+ * clone方法：克隆当前对象，结果为Object类型。
+ * */
+public class ComplexNumber implements Cloneable{
 	private double realPart;//复数的实部
 	private double imaginaryPart;//复数的虚部
 	
+	
+	/**
+	 * 根据现有对象克隆一个新对象，新对象和现有的对象的值是一样的
+	 * */
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		
+		try {
+			//克隆一个对象时，应该先调用父类的clone方法
+			ComplexNumber newObject = (ComplexNumber)super.clone();
+			//将现有对象的值赋值给新对象
+			newObject.setRealPart(this.realPart);
+			newObject.setImaginaryPart(this.imaginaryPart);
+			return newObject;
+		} catch (CloneNotSupportedException e) {
+			//如果没有实现Cloneable接口，则抛出异常
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	/**
+	 * 比较一个对象的值是否和这个复数对象的值相等
+	 * */
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null){
+			return false;
+		}
+		//首先判断a是不是一个复数对象，instanceof 关键字是用来判断对象的类型的
+		if(obj instanceof ComplexNumber){
+			//如果a是复数对象，则将它强制类型转换成复数对象，以调用复数类提供的方法
+			ComplexNumber b = (ComplexNumber)obj;
+			if((this.realPart == b.getRealPart()) && (this.imaginaryPart == b.getImaginaryPart())){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
+	/**
+	 * 获取该复数对象的hash码
+	 * */
+	@Override
+	public int hashCode() {
+		//如果两个复数对象的值是相等的，那么他们的hash码对象必须相同
+		//两个值相等的复数对象通过toString()方法得到的输出字符串是一样的
+		//于是，可以把得到的字符串的hash码当做复数对象的hash码
+		return this.toString().hashCode();
+	}
 	/*默认构造函数*/
 	public ComplexNumber(){
 		this.realPart = 0.0;
@@ -107,15 +164,21 @@ public class ComplexNumber {
 		return this.realPart + "+" + this.imaginaryPart + "i";
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CloneNotSupportedException {
 		ComplexNumber a = new ComplexNumber(2, 4);
-		ComplexNumber b = new ComplexNumber(1, 2);
+		ComplexNumber b = new ComplexNumber(2, 4);
 		System.out.println("ComplexNumber a：" + a.toString());
 		System.out.println("ComplexNumber b:" + b.toString());
 		System.out.println("(a + b) = " + a.add(b).toString());
 		System.out.println("(a - b) = " + a.decrease(b).toString());
 		System.out.println("(a * b) = " + a.add(b).toString());
 		System.out.println("(a / b) = " + a.decrease(b).toString());
+		//用自定义的equals方法比较两个对象是否相等
+		System.out.println("a.equals(b) =" + a.equals(b));
+		//用自定义的hashCode方法获取对象的hash码
+		System.out.println("a.hashCode(b) =" + a.hashCode() + "b.hashCode()" + b.hashCode());
+		//使用自定义的clone对象克隆对象
+		System.out.println("a.clone = " + a.clone().toString());
 	}
 
 }
