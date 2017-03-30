fa0558c65a6f160756229ae21c73c9ba83613aeb
forever-xiaohao <1012958103@qq.com>
2017-03-30 21:27:56 +0800

    自定义图形的基础类

---
+++ java_jdk7_ch2/src/book/oo/shape/MyShape.java
@@ -0,0 +1,29 @@
+package book.oo.shape;
+
+public abstract class MyShape {
+	/**形状的名字*/
+	protected String name;//protected访问控制符表示只有本类和子类能够访问该属性
+	/**抽象方法，获取形状的周长*/
+	public abstract double getGirth();
+	/**抽象方法，获取形状的面积*/
+	public abstract double getArea();
+	/**抽象方法，输出形状*/
+	public abstract String toString();
+	
+	/**
+	 * 获取图形的名字
+	 * @return 返回图形的名字
+	 * */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * 为图形设置名字
+	 * @param name 要设置的形状的名字
+	 * */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
